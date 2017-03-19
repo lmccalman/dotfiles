cb8c6b236fc18f480992f8c891eabd0d0566de99
Lachlan McCalman <lachlan.mccalman@data61.csiro.au>
2017-03-19 18:57:24 +1100

    changes for htpc

--- .init-wm.sh
+++ .init-wm.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 xrdb -merge -I$HOME ~/.Xresources
-nm-applet --sm-disable &
 unclutter -grab -noevents -root &
 compton --config ~/.compton.conf &
-$HOME/.dotfiles/bin/rotatingwallpaper &
 enpass &
 
--- .profile
+++
@@ -1,3 +0,0 @@
-export QT_STYLE_OVERRIDE=adwaita
-export VDPAU_DRIVER=va_gl
-source ~/.private
