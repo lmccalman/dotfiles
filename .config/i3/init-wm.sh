22e070f4738354ea849d3c9ed266b2986f36f943
Lachlan McCalman <lachlan.mccalman@data61.csiro.au>
2017-04-23 14:22:06 +1000

    fix cursor

--- .config/i3/init-wm.sh
+++ .config/i3/init-wm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
 xrdb -merge -I$HOME ~/.Xresources
+xsetroot -cursor_name left_ptr
 unclutter -grab -noevents -root &
 compton --config ~/.compton.conf &
 $HOME/.config/i3/wallpaper.sh &
