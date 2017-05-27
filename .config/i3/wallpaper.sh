87160008d724038bd90c9c662490481c26d3aa22
Lachlan McCalman <lachlan.mccalman@data61.csiro.au>
2017-05-27 20:15:06 +1000

    Change wallpaper path

--- .config/i3/wallpaper.sh
+++ .config/i3/wallpaper.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 while true; do
-    find ~/images/wallpapers -type f \( -name '*.jpg' -o -name '*.png' -o -name '*.jpeg' \) -print0 |
+    find ~/Dropbox/images/wallpapers/UHD -type f \( -name '*.jpg' -o -name '*.png' -o -name '*.jpeg' \) -print0 |
         shuf -n1 -z | xargs -0 feh --bg-fill
     sleep 4h
 done
