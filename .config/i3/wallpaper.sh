cd8dc6fdb63e7dad5d90bd172c566f2eea44b923
Lachlan McCalman <lachlan@mccalman.info>
2021-07-16 09:30:00 +1000

    use store folder

--- .config/i3/wallpaper.sh
+++ .config/i3/wallpaper.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 while true; do
-  IMAGES=`find ~/Dropbox/images/wallpapers/UHD -type f \( -name '*.jpg' -o -name '*.png' -o -name '*.jpeg' \) | shuf -n2`
+  IMAGES=`find ~/store/Dropbox/images/wallpapers/UHD -type f \( -name '*.jpg' -o -name '*.png' -o -name '*.jpeg' \) | shuf -n2`
   IMAGE1=`echo ${IMAGES} | sed -n 1p`
   IMAGE2=`echo ${IMAGES} | sed -n 2p`
   feh --bg-fill ${IMAGE1} --bg-fill ${IMAGE2}
